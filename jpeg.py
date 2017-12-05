103f8237fe87a8535f50ee6d8b1b4706765dfa2f
Nicolás Pazos <npazosmendez@gmail.com>
2017-12-05 19:27:19 -0300

    Ahora hay una clase jpeg, y arreglado un error que hacia que cambien las domensiones de las imagenes

--- jpeg.py
+++ jpeg.py
@@ -14,33 +14,32 @@ import numpy as np
 from scipy.fftpack import dct, idct
 from libs.huffman import *
 
+class jpeg:
+    def __init__(self, binstring, hufftree, height, width, NM = (8,8), \
+                QTable = np.array([\
+                    [16,11,10,16,24,40,51,61],\
+                    [12,12,14,19,26,58,60,55],\
+                    [14,13,16,24,40,57,69,56],\
+                    [14,17,22,29,51,87,80,62],\
+                    [18,22,37,56,68,109,103,77],\
+                    [24,35,55,64,81,104,113,92],\
+                    [49,64,78,87,103,121,120,101],\
+                    [72,92,95,98,112,100,103,99],\
+                    ])):
+        assert(len(QTable) == NM[0])
+        assert(len(QTable[0]) == NM[1])
+        self.binstring = binstring
+        self.height = height
+        self.width = width
+        self.data = binstring
+        self.hufftree = hufftree
+        self.N = NM[0]
+        self.M = NM[1]
+        self.QTable = QTable
 
-def block_partition(img, N = 8, M = None):
-    """
-    Particiona la imagen en bloques de N x M
-    * Input:
-        img : array bidimensional (img de un solo canal)
-        N : natural (alto de bloque, 8 por defecto)
-        M : natural (ancho de bloque, cuadrado por defecto)
+    def size(self):
+        return len(self.binstring)/8/1000
 
-    * Output: array de '(Height/N * Width/N) x N x M' (array de matrices)
-    """
-    if M == None:
-        # Por defecto es cuadrado
-        M = N
-    res = np.array([img[x:x+M,y:y+N] for x in range(0,img.shape[0]-M+1,M) for y in range(0,img.shape[1]-N+1,N)])
-    return res
-
-def merge_blocks(blocks, alto, ancho, N = 8, M = None):
-    if M == None:
-        # Por defecto es cuadrado
-        M = N
-    res = []
-    for x in range(0,len(blocks),ancho//M):
-        res.append(np.concatenate(blocks[x:x+ancho//M],axis=1))
-    res = np.array(res)
-    res = np.concatenate(res,axis=0)
-    return res
 
 def dct2(block):
     return dct(dct(block.T, norm='ortho').T, norm='ortho')
@@ -48,39 +47,32 @@ def dct2(block):
 def idct2(block):
     return idct(idct(block.T, norm='ortho').T, norm='ortho')
 
-def jpeg_encode(img, N = 8, M = None, QTable = None):
+def jpeg_encode(img, NM = (8,8), QTable = np.array([\
+        [16,11,10,16,24,40,51,61],\
+        [12,12,14,19,26,58,60,55],\
+        [14,13,16,24,40,57,69,56],\
+        [14,17,22,29,51,87,80,62],\
+        [18,22,37,56,68,109,103,77],\
+        [24,35,55,64,81,104,113,92],\
+        [49,64,78,87,103,121,120,101],\
+        [72,92,95,98,112,100,103,99],\
+        ])):
     """
     Codifica una imagen en mapa de bits a JPEG.
     * Input:
         img: array bidimensional (img de un solo canal, por ahora)
-        N : natural (alto de bloque de partición, 8 por defecto)
-        M : natural (ancho de bloque de partición, cuadrado por defecto)
+        NM : tupla de naturales (tamaño de bloque, (8,8) por defecto)
         QTable: array bidimensional de NxM (tabla de cuantización de coeficientes)
-    * Output:
-        por verse...
+    * Output: instancia de la clase jpeg
     """
-    # Establezo los valores por defecto
-    if M == None:
-        M = N
-    if QTable == None:
-        if not(N == 8 and M == 8):
-            print('Falta tabla de cuantización.')
-            exit(1)
-        QTable = np.array([\
-                [16,11,10,16,24,40,51,61],\
-                [12,12,14,19,26,58,60,55],\
-                [14,13,16,24,40,57,69,56],\
-                [14,17,22,29,51,87,80,62],\
-                [18,22,37,56,68,109,103,77],\
-                [24,35,55,64,81,104,113,92],\
-                [49,64,78,87,103,121,120,101],\
-                [72,92,95,98,112,100,103,99],\
-                ])
-    assert(len(QTable) == N)
-    assert(len(QTable[0]) == M)
+    assert(len(QTable) == NM[0])
+    assert(len(QTable[0]) == NM[1])
 
-
+    N = NM[0]
+    M = NM[1]
     # 1-padding para que altura % N = 0 y ancho % M = 0
+    alto = img.shape[0]
+    ancho = img.shape[1]
     if not(img.shape[0] % N == 0):
         unos = np.ones((N-img.shape[0]%N,img.shape[1]),dtype=np.int8)
         img = np.concatenate((img, unos), axis=0)
@@ -153,55 +145,36 @@ def jpeg_encode(img, N = 8, M = None, QTable = None):
     print('Tamaño en Kbytes original: \t', img.shape[0]*img.shape[1]*8 / 8 / 1000)
     print('Tamaño en Kbytes comprimido: \t', len(binstring) / 8 / 1000)
 
-    return (binstring, hufftree)
+    res = jpeg(binstring, hufftree, alto, ancho, (N,M), QTable)
 
+    return res
 
-def jpeg_decode(binstring, hufftree, alto, ancho, N = 8, M = None, QTable = None):
+def jpeg_decode(jpeg):
     """
     Decodifica una imagen de JPEG a mapa de bits.
     * Input (sujeto a modificaciones):
-        binstring: string de 0/1 (datos de la imagen codificados)
-        hufftree: árbol de codificación de Huffman
-        N : natural (alto de bloque de partición, 8 por defecto)
-        M : natural (ancho de bloque de partición, cuadrado por defecto)
-        QTable: array bidimensional de NxM (tabla de cuantización de coeficientes)
+        jpeg: instancia de la clase jpeg
     * Output: imagen en mapa de bits
     """
-    # Establezo los valores por defecto
-    if M == None:
-        M = N
-    if QTable == None:
-        if not(N == 8 and M == 8):
-            print('Falta tabla de cuantización.')
-            exit(1)
-        QTable = np.array([\
-                [16,11,10,16,24,40,51,61],\
-                [12,12,14,19,26,58,60,55],\
-                [14,13,16,24,40,57,69,56],\
-                [14,17,22,29,51,87,80,62],\
-                [18,22,37,56,68,109,103,77],\
-                [24,35,55,64,81,104,113,92],\
-                [49,64,78,87,103,121,120,101],\
-                [72,92,95,98,112,100,103,99],\
-                ])
-    assert(len(QTable) == N)
-    assert(len(QTable[0]) == M)
+    binstring = jpeg.binstring
+    hufftree = jpeg.hufftree
+    alto = jpeg.height
+    ancho = jpeg.width
+    N = jpeg.N
+    M = jpeg.M
+    QTable = jpeg.QTable
 
     # Tomo alturas auxiliares, por si hay padding
-    alto_bu = alto
-    ancho_bu = ancho
     if not(alto % N == 0):
-        alto += N-alto%N
+        alto = alto + N-alto%N
     if not(ancho % M == 0):
-        ancho += M-ancho%M
+        ancho = ancho + M-ancho%M
 
     # Decodificación entrópica
     print('Descomprimiendo...')
     seq_comp = huffman_uncompress(binstring, hufftree)
 
     # Descompresión de secuencia
-    # cada simbolo K se reduce a una tupla (C,k), donde C indica la cantidad
-    # de ceros que tiene antes (por bloque)
     seq = []
     cuantos = 0
     for (C,k) in seq_comp:
@@ -256,7 +229,7 @@ def jpeg_decode(binstring, hufftree, alto, ancho, N = 8, M = None, QTable = None
     img = img + unos*128
 
     # Si hubo padding, se lo quito
-    img = img[:alto_bu,:ancho_bu]
+    img = img[0:jpeg.height,0:jpeg.width]
 
     print('Decodificación finalizada.')
     return img
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 import numpy as np
-img = io.imread('bmp/lena.bmp')[:,:,0]
+img = io.imread('bmp/sun.bmp')[:,:,0]
 
 print("Dimensiones originales:",img.shape)
 
@@ -16,14 +16,14 @@ plt.title('Sin comprimir\n size: '+str(len(img)*len(img[0])/1000)+' KB')
 
 # QTable = [[15,50],[70,200]]
 # Codifico en JPEG
-(binstring, hufftree) = jpeg_encode(img,N=8)
+jpeg = jpeg_encode(img,(8,8))
 # Decodifico
-img = jpeg_decode(binstring, hufftree,len(img),len(img[0]),N=8)
+img = jpeg_decode(jpeg)
 
 print("Dimensiones finales:",img.shape)
 
 plt.figure()
 plt.imshow(img,cmap='gray')
-plt.title('Comprimida con JPEG\n size: '+str(len(binstring)/8/1000)+' KB')
+plt.title('Comprimida con JPEG\n size: '+str(jpeg.size())+' KB')
 
 plt.show()
